cf5c98693130d3151fd6bfdf09df90f7857de04b
Phong <phongdoan.dev@gmail.com>
2019-12-12 14:24:21 -0800

    Datebase works

--- server.js
+++ server.js
@@ -25,30 +25,16 @@ client.on('error', error => console.log(error));
 client.connect();
 
 // LOCATION PATH
-app.get('/location', (request, res) => {
-  let query = request.query.data;
-  checkDatabase(query);
-//   superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GEOCODE_API_KEY}`).then(response => {
-//     const location = response.body.results[0].geometry.location;
-//     const formAddr = response.body.results[0].formatted_address;
-//     const searchquery = response.body.results[0].address_components[0].long_name.toLowerCase();
-//     if (query !== searchquery) {
-//       response.send(error);
-//       console.log(error);
-//       return null;
-//     }
-//     locationSubmitted = new Geolocation(searchquery, formAddr, location);
-//     res.send(locationSubmitted);
-//   })
+app.get('/location', checkDatabase);
 
-});
 // LOCATION CONSTRUCTOR FUNCTION
-function Geolocation(searchquery, formAddr, location) {
+function Geolocation(searchquery, formAddr, lat, lng) {
   this.searchquery = searchquery;
   this.formatted_query = formAddr;
-  this.latitude = location['lat'];
-  this.longitude = location['lng'];
+  this.latitude = lat;
+  this.longitude = lng;
 }
+
 // WEATHER PATH
 app.get('/weather', (request, response) => {
   superagent.get(`https://api.darksky.net/forecast/${WEATHER_API_KEY}/${locationSubmitted.latitude},${locationSubmitted.longitude}`).then(res => {
@@ -72,13 +58,13 @@ app.get('/events', (request, response) => {
       return new Event(event.url, event.title, event.start_time, event.description)
     })
     response.send(eventData);
-  }).catch( function () {
+  }).catch(function () {
     console.log('banana');
     return null;
   })
 })
 
-function Event(link, name, event_date, summary='none') {
+function Event(link, name, event_date, summary = 'none') {
   this.link = link,
   this.name = name,
   this.event_date = event_date,
@@ -92,15 +78,47 @@ function Event(link, name, event_date, summary='none') {
 //       res.send(sqlResponse.rows);
 //     });
 // when the user enters the query I want user to check to database to see if the queried info is there already
-function checkDatabase(userInput) {
-    const sql = 'SELECT * FROM cityLocation;';
-    client.query(sql).then(sqlResponse => {
-
-        console.log(sqlResponse.rows[0].searchquery);
-        
-    });
-} 
-
+function checkDatabase(request, response) {
+  let query = request.query.data;
+  console.log('query :', query);
+  const sql = 'SELECT * FROM cityLocation;';
+  client.query(sql).then(sqlResponse => {
+    if (sqlResponse.rows.length < 1) {
+      createDataFromAPI(request, response, query);
+    } else {
+      sqlResponse.rows.forEach(location => {
+        console.log('location :', location);
+        if (location.searchquery === query) {
+          locationSubmitted = new Geolocation(location.searchquery, location.formattedQuery, location.latitude, location.longitude);
+          response.send(locationSubmitted);
+        } else {
+          createDataFromAPI(request, response, query);
+        }
+      });
+    }
+  });
+}
+function createDataFromAPI(request, response, query) {
+  superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${GEOCODE_API_KEY}`).then(geoResponse => {
+    const location = geoResponse.body.results[0].geometry.location;
+    const formAddr = geoResponse.body.results[0].formatted_address;
+    const searchquery = geoResponse.body.results[0].address_components[0].long_name.toLowerCase();
+    locationSubmitted = new Geolocation(searchquery, formAddr, location.lat, location.lng);
+    const SQL = `INSERT INTO cityLocation(
+            searchQuery,
+            formattedQuery,
+            latitude,
+            longitude
+          ) VALUES(
+            $1, 
+            $2,
+            $3,
+            $4
+          )`;
+    client.query(SQL, [locationSubmitted.searchquery, locationSubmitted.formatted_query, locationSubmitted.latitude, locationSubmitted.longitude]);
+    response.send(locationSubmitted);
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`App is on PORT: ${PORT}`);
